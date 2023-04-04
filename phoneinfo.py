4597ee9d7c2302b6e46e6dc4d16db409fe7eb761
aHacKEr12345 <127112818+aHacKEr12345@users.noreply.github.com>
2023-04-04 12:20:18 +0200

    Update phoneinfo.py

--- phoneinfo.py
+++ phoneinfo.py
@@ -1,35 +1,21 @@
-# Import the requests library
 import requests
 
-# Define a function that takes a phone number and an optional country code
-def phone_number_info(number, country_code=None):
-    # Set the API URL
+def phone_number_info(phone_number, country_code=None):
     url = "http://apilayer.net/api/validate"
-
-    # Set the API parameters
     params = {
         "access_key": "oA6417lDKB4gWwA04ph1Id9rqdE6QISN",
-        "number": number,
+        "number": phone_number,
         "country_code": country_code
     }
-
-    # Make the API call and get the response
     response = requests.get(url, params=params)
-
-    # Get the JSON data from the response
     data = response.json()
-
-    # Check if the number is valid
-    if data["valid"]:
-        # Print information about the phone number
-        print("The number is valid.")
+    if "valid" in data and data["valid"]:
+        print("The phone number is valid")
         print(f"Country code: {data['country_code']}")
         print(f"Country name: {data['country_name']}")
-        print(f"Location: {data['location']}")
+        print(f"City: {data['location']}")
         print(f"Carrier: {data['carrier']}")
     else:
-        # Print an error message if the number is invalid
-        print("The number is invalid.")
+        print("The phone number is invalid")
 
-# Call the function with a phone number and a country code
 phone_number_info("698889903", "355")
